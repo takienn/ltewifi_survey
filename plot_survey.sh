90dcf6184b1de424cb06d391d883ca93f5bff8fe
Taqi Ad-Din <takai.kenn@gmail.com>
2017-01-25 19:13:09 +0100

    Disable unnecessary info

--- plot_survey.sh
+++ plot_survey.sh
@@ -12,6 +12,9 @@ old_cb=0
 old_cr=0
 old_ct=0
 ct_offset=0
+
+rm plot.dat;
+
 while true; do
 	survey="$(iw dev $dev survey dump|grep -A 5 in\ use| sed 's/[^0-9]*//g')";
 	freq=$(echo $survey | cut -f1 -d\ );
@@ -23,28 +26,29 @@ while true; do
 	channel_busy=$(echo $survey | cut -f4 -d\ );
 	channel_rx=$(echo $survey | cut -f5 -d\ );
 	channel_tx=$(echo $survey | cut -f6 -d\ );
-	printf "Frequency $freq\nNoise Floor $noise_f\nChannel Time $channel_time\nChannel Busy $channel_busy\nChannel Rx $channel_rx\nChannel Tx $channel_tx\n"
+	printf "Frequency $freq\nNoise Floor $noise_f\n" #Channel Time $channel_time\nChannel Busy $channel_busy\nChannel Rx $channel_rx\nChannel Tx $channel_tx\n"
 	busy=$(bc <<< "scale=3; 100*$channel_busy/$channel_time")
 	tx=$(bc <<< "scale=3; 100*$channel_tx/$channel_time")
 	rx=$(bc <<< "scale=3; 100*$channel_rx/$channel_time")
 	dif_ctime=$(bc <<< "scale=3; $channel_time-$old_ctime")
 	dif_cb=$(bc <<< "scale=3; $channel_busy-$old_cb")
-	#dif_cr=$(bc <<< "scale=3; $channel_rx-$old_cr")
-	#dif_ct=$(bc <<< "scale=3; $channel_tx-$old_ct")
+	dif_cr=$(bc <<< "scale=3; $channel_rx-$old_cr")
+	dif_ct=$(bc <<< "scale=3; $channel_tx-$old_ct")
 	dif_busy=$(bc <<< "scale=3; 100*$dif_cb/$dif_ctime")
-	#dif_rx=$(bc <<< "scale=3; 100*$dif_cr/$dif_ctime")
-	#dif_tx=$(bc <<< "scale=3; 100*$dif_tx/$dif_ctime")
+	dif_rx=$(bc <<< "scale=3; 100*$dif_cr/$dif_ctime")
+	dif_tx=$(bc <<< "scale=3; 100*$dif_ct/$dif_ctime")
 
-	printf "Channel Busy %%: $busy\n"
-	printf "Channel Busy (dif) %%: $dif_busy\n"
+	#printf "Channel Busy %%: $busy\n"
+	printf "Channel Busy %%: $dif_busy%% dwell time $dif_ctime ms\n"
 
 	old_ctime=$channel_time
 	old_cb=$channel_busy
-	#old_cr=$channel_rx
-	#old_ct=$channel_tx
+	old_cr=$channel_rx
+	old_ct=$channel_tx
 
 	relative_time=$(bc <<< "scale=3;$channel_time/1000-$ct_offset/1000")
 	echo $relative_time
-	printf "$channel_time\t$dif_busy\n" >> plot.dat
+	printf "$relative_time\t$dif_busy\n" >> plot.dat
+	
 	sleep 1
 done
